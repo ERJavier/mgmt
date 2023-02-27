7347f3dc30488fc0964aa802ca63f6298fd7becd
Javier Cepeda <erjavierc@gmail.com>
2023-02-27 09:49:47 -0500

    made schema for mongoose

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,4 +1,7 @@
-const { projects, clients } = require('../sampleData');
+//MONGOOSE MODELS 
+const Project = require('../models/Project');
+const Client = require("../models/Client");
+
 
 const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList } = require("graphql");
 
@@ -13,7 +16,7 @@ const ProjectType = new GraphQLObjectType({
     client: {
         type: ClientType,
         resolve(parent, args) {
-            return clients.find(client => client.id === parent.clientId)
+            return Client.findById(parent.clientId)
         }
     }
   }),
@@ -36,27 +39,27 @@ const RootQuery = new GraphQLObjectType({
     projects: {
         type: new GraphQLList(ProjectType),
         resolve(parrent, args) {
-            return projects
+          return Project.find();
         }
     },
     project: {
         type: ProjectType,
         args: { id: { type: GraphQLID }},
         resolve(parrent, args) {
-            return projects.find(project => project.id === args.id)
+            return Project.findById(args.id)
         }
     },
     clients: {
         type: new GraphQLList(ClientType),
         resolve(parrent, args) {
-            return clients
+            return Client.find()
         }
     },
     client: {
         type: ClientType,
         args: { id: { type: GraphQLID }},
         resolve(parrent, args) {
-            return clients.find(client => client.id === args.id)
+            return Client.findById(args.id)
         }
     }
   }
