aa9122be8fa22d862f1120425ec0d9f74c42db2c
Javier Cepeda <erjavierc@gmail.com>
2023-02-27 15:28:09 -0500

    add client done √

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,31 @@
 import Header from "./components/Header";
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import Clients from "./components/Clients";
+import AddClientModel from "./AddClientModel";
+
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incomong) {
+            return incomong;
+          },
+        },
+        projects: {
+          merge(existing, incomong) {
+            return incomong;
+          },
+        },
+      },
+    },
+  },
+});
 
 const client = new ApolloClient({
   uri: 'http://localhost:5001/graphql',
-  cache: new InMemoryCache(),
+  cache,
 })
 
 function App() {
@@ -13,6 +34,7 @@ function App() {
       <ApolloProvider client={client}>
         <Header />
         <div className="container">
+          <AddClientModel />
           <Clients />
         </div>
       </ApolloProvider>
